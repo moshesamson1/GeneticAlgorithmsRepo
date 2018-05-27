d6faa0020671f208f9cd5763bb1fe5e70ae7bec3
Moshe Samson <samson.moshe@gmail.com>
2018-05-27 20:03:13 +0300

    implement back propagation on wheat_seeds example

--- GeneticAlgorithmsExercise/MNIST_Loader.py
+++ GeneticAlgorithmsExercise/MNIST_Loader.py
@@ -1,5 +1,12 @@
 import mnist
-
+from mnist import MNIST
 
 def get_train_images():
-    return mnist.train_images()
+    mndata = MNIST('resources')
+    mndata.gz = True
+    images, labels = mndata.load_training()
+    return images, labels
+
+if __name__ == "__main__":
+    images, labels = get_train_images()
+    print images[1]
